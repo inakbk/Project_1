e1d53eb53ce1d20f21a442ca8d499a4ee7cf792a
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-10 12:01:28 +0200

    moved to correct folder

---
+++ exercise_b/main.cpp
@@ -0,0 +1,27 @@
+//Exercise b, algorithm for solving the set of linear equations
+//a_i*v_(i-1) + b_i*v_i + c_i*v_(i+1) = tilde_b_i
+
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+int main()
+{
+    const int n = 10;
+    const double h = 1./(n+1);
+    vec a =
+
+    //creating vectors a, b, c:
+    for(int Index = 0; Index < n; ++Index)
+    {
+
+    }
+
+
+    //vec x = randu<vec>(5);
+
+    return 0;
+}
+
