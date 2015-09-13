6c8b7e85ff1d67e178431352b59efd3a953b2fb6
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-14 01:38:56 +0200

    plotting n=10**8 for fun

--- exercise_b/project_1_run_file.py
+++ exercise_b/project_1_run_file.py
@@ -36,8 +36,9 @@ def u(x):
 ------------------------------------------------------------------------------------------
 """
 
-N = [10, 100, 1000, 10**4]#, 10**5]#, 10**6, 10**7, 10**8] #the highest n only works for the subst. method
+N = [10, 100, 1000, 10**4, 10**5, 10**6, 10**7, 10**8] #the highest n only works for the subst. method
 h = zeros(len(N))
+h_lu = zeros(len(N))
 relative_error = zeros(len(N))
 relative_error_lu = zeros(len(N))
 time_diag = zeros(len(N))
@@ -48,49 +49,51 @@ for n in N:
 	"""
 	Run c++ code from here with n as command line argument:
 	"""
-	os.system('c++ -o main main.cpp')
-	os.system('./main %s' %n)
+	#os.system('c++ -o main main.cpp')
+	#os.system('./main %s' %n)
 
 	"""
 	Fetching file from c++ code
 	C++ code with LU decomposition needs to be run in QT Creator first to avoid (weird) compilation error..
 	"""
+	print 'noe', n
 	filename_subst = 'linear_eq_solution_reduced_n%s.txt' %n
-	filename_lu = '/Users/Ina/build/build-exercise_d-Desktop_Qt_5_5_0_clang_64bit-Debug/linear_eq_solution_lu_n%s.txt' %n
-
 	x, v, time_diag[i-1] = read_file(filename_subst)
-	x_lu, v_lu, time_lu[i-1] = read_file(filename_lu)
-	
-	figure(i)
-	plot(x,v, 'b')
-	hold('on')
-	plot(x_lu,v_lu, 'g')
-	plot(x,u(x), 'r')
-	legend(['Numerical subst.','Numerical lu dec.', 'Analytic solution'], fontsize=18, loc='upper right')
-	xlabel('x', fontsize=18)
-	ylabel('Solution', fontsize=18)
-	title('Plot of analytic and numerical solutions with n=%s ' %n, fontsize=18) 
-	savefig('linear_eq_solution_plot_all_num_n%s.eps' %n)
-	hold('off')
 
+	if n < 10**5:
+		filename_lu = '/Users/Ina/build/build-exercise_d-Desktop_Qt_5_5_0_clang_64bit-Debug/linear_eq_solution_lu_n%s.txt' %n
+		x_lu, v_lu, time_lu[i-1] = read_file(filename_lu)
+		relative_error_lu[i-1] = mean((abs((v_lu - u(x))/u(x)))) 
+		h_lu[i-1] = 1./(n+1)
+
+	
 	"""
-	------------------------------------------------------------------------------------------
-	Exercise c,d
+		figure(i)
+		plot(x,v, 'b')
+		hold('on')
+		plot(x_lu,v_lu, 'g')
+		plot(x,u(x), 'r')
+		legend(['Numerical subst.','Numerical lu dec.', 'Analytic solution'], fontsize=18, loc='upper right')
+		xlabel('x', fontsize=18)
+		ylabel('Solution', fontsize=18)
+		title('Plot of analytic and numerical solutions with n=%s ' %n, fontsize=18) 
+		savefig('linear_eq_solution_plot_all_num_n%s.eps' %n)
+		hold('off')
 	"""
 
 	relative_error[i-1] = mean((abs((v - u(x))/u(x)))) #extraxting rel. error for each n
-	relative_error_lu[i-1] = mean((abs((v_lu - u(x))/u(x)))) 
-
+	
 	h[i-1] = 1./(n+1)
 	i += 1
 
+print 'ssss'
 figure(12)
 subplot(211)
 grid('on')
 loglog(h, relative_error, 'ko-')
 hold('on')
-loglog(h, relative_error_lu, 'ro-')
-legend(['Relative error subst.', 'Relative error lu dec.'], fontsize=14, loc='upper left')
+loglog(h_lu, relative_error_lu, 'ro-')
+legend(['Relative error subst.', 'Relative error lu dec.'], fontsize=14, loc='upper right')
 xlabel('h', fontsize=16)
 ylabel('Relative error', fontsize=16)
 title('Relative error for both numerical methods', fontsize=16) 
@@ -100,7 +103,7 @@ hold('off')
 subplot(212)
 loglog(h, time_diag, 'ko-')
 hold('on')
-loglog(h, time_lu, 'ro-')
+loglog(h_lu, time_lu, 'ro-')
 legend(['Time subst.', 'Time lu dec.'], fontsize=14, loc='upper right')
 xlabel('h', fontsize=16)
 ylabel('Time [sec]', fontsize=16)
@@ -109,9 +112,11 @@ savefig('linear_eq_time_plot_all_num_N%s.eps' %N[-1])
 
 tight_layout()
 
+print 'llll'
+
 show()
 
-
+print 'hhh'
 
 
 
