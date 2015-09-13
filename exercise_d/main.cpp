938e97897d2294d49005be8939dadc97bc7a8b8b
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-13 18:26:28 +0200

    structuring into two programs
    one for lu dec and one for subst. to avid compilation error for lu

--- exercise_d/main.cpp
+++ exercise_d/main.cpp
@@ -1,6 +1,5 @@
 /*Exercise b, algorithm for solving the set of linear equations
- * with substitution and timing it
- * a_i*v_(i-1) + b_i*v_i + c_i*v_(i+1) = h*h*f(x_i)*/
+ * with lu factorisation and timing it*/
 
 #include <iostream>
 #include <armadillo>
@@ -32,37 +31,44 @@ void MakePlotFile(const vec x, const vec solution, const int n, const double tim
         cout << "Datafile done for n=" << n << endl;
 }
 
-void Forward_Backward_Substitution(vec b, vec b_thilde, const vec x, const int n)
+//solving with armadillos lu dec.
+void solve_lu(vec b, vec b_thilde, const vec x, const int n)
 {
+    int a = -1.;
+    int c = -1.;
+    //creating A, L, U and making shure b_thilde has right dim.:
+    mat A = zeros<mat>(n,n);
+    mat L = zeros<mat>(n,n);
+    mat U = zeros<mat>(n,n);
+    b_thilde.shed_row(0);
+    b_thilde.shed_row(n);
+    for(int i=0, j=1; (i<=n-1) && (j<=n-1); ++i, ++j)
+    {
+        A(i,j) = c;
+        A(i,i) = b[i+1];
+        A(j,i) = a;
+    }
+    A(n-1,n-1) = b[n];
+
     //clocking the operations (only solve, not making file):
-    clock_t start_diag, finish_diag; //declaring start and finish time
-    start_diag = clock();
+    clock_t start_lu, finish_lu; //declaring start and finish time
+    start_lu = clock();
 
-    //forward substitution:
-    for(int i = 2; i < n+1; ++i) //starting from second row till last
-    {
-        b[i] = b[i] - 1/b[i-1]; //b' one iteration
-        b_thilde[i] = b_thilde[i] + b_thilde[i-1]/b[i-1];
-    }
-
-    //backward substitution:
-    for(int i = n; i > 1; --i) //starting from last row n to i==1
-    {
-        b_thilde[i-1] = b_thilde[i-1] + b_thilde[i]/b[i];
-    }
-
-    /*Final solution, dividing by b[i-1] after backward sust. to
-     *  find final solution v */
-    vec v = b_thilde/b;
+    //arma::lu(L,U,A);
+    //size(L).print();
+    //vec y = solve(L,b_thilde);
+    //size(y).print();
+    //vec v = solve(U,y);
+    //v.print();
 
     //stopping timer:
-    finish_diag = clock();
-    double time_diag = ( (finish_diag - start_diag)/((double)CLOCKS_PER_SEC ) );
-    cout << "Substitution: Time for n=" << n << ":  " << time_diag << "seconds" << endl;
+    finish_lu = clock();
+    double time_lu = ( (finish_lu - start_lu)/((double)CLOCKS_PER_SEC ) );
+    cout << "LU solve: Time for n=" << n << ":  " << time_lu << " seconds" << endl;
 
     //Sending the vectors to file:
-    MakePlotFile(x, v, n, time_diag, "reduced"); //making plot file
-    //v.print();
+    //MakePlotFile(x, v, n, time_lu, "lu"); //making plot file
+
 }
 
 int main(int argc, char *argv[])
@@ -87,11 +93,8 @@ int main(int argc, char *argv[])
         vec x = linspace<vec>(0, 1, n+2);
         vec b_thilde = h*h*f(x);
 
-        /*Solving equations by forward/backward substitution:
-         * (b, b_thilde, x and n not changed in main() so they can be
-         * used for next method) */
-        Forward_Backward_Substitution(b, b_thilde, x, n);
-        //cout << "-----" << endl;
+        //Using the armadillo library with LU decomposition to solve the equations:
+        solve_lu(b, b_thilde, x, n);
 
     }
 
